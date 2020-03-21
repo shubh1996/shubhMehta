8f6bb20ea581901a7d9fc0e4f2598e6d68342176
shubh1996 <shubh.mehta@maistering.com>
2020-03-22 00:45:27 +0530

    init commit

---
+++ js/calc.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    
+
+    $('.dp-image').hover(function () {
+        $('.icons-box').css("height", "30vh");
+    });
+    $('.icons-box').hover(function(){},function () {
+        $('.icons-box').css("height", "0");
+    });
+    
+});
